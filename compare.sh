cb441353e88cb58dfb08e30c6126b8fcfcaa8be2
Michael Wiebusch <m.wiebusch@gsi.de>
2017-11-01 14:08:23 +0100

    juelich_ana branch can now also deal with threshold scans ... very dirty fix in compare.sh

--- compare.sh
+++ compare.sh
@@ -4,10 +4,17 @@ scan_dir=$1
 export outdir=$(echo $scan_dir | sed 's/data/ana/')
 mkdir -p $outdir
 
+rm intensity_list.txt
 if [ -e $scan_dir/thr_list.txt ]; then
   echo "is a threshold scan"
   export scan_thr="true"
   cp $scan_dir/thr_list.txt thr_list.txt
+  cp $scan_dir/thr_list.txt intensity_list.txt
+elif [ -e $scan_dir/thr_list_pt.txt ]; then
+  echo "is a threshold scan"
+  export scan_thr="true"
+  cp $scan_dir/thr_list_pt.txt thr_list.txt
+  cp $scan_dir/thr_list_pt.txt intensity_list.txt
 else 
   cut -d " " -f 1 $scan_dir/point_list.txt > xlist.txt
   cut -d " " -f 2 $scan_dir/point_list.txt > ylist.txt
@@ -16,7 +23,6 @@ fi
 
 find $scan_dir -name "*.hld.root" | sort > root_files.txt
 
-rm intensity_list.txt
 # for i in $(find $scan_dir -name "laser_intensity.txt" | sort); do
 # echo $(cat $i) | sed 's/,/./' >> intensity_list.txt
 # done
